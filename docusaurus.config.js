f1f9cee1e46922c0f4d6daa6153dd2e782160270
yokinanya <1099332511@qq.com>
2022-11-08 14:19:54 +0800

    修复了写错文档仓库地址的Bug（确信）

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,13 +36,13 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/yokinanya/0StarFeathers_Docs/tree/master/docs',
+          editUrl: 'https://github.com/yokinanya/0StarFeathers_Docs/tree/master',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/yokinanya/0StarFeathers_Docs/tree/master/blog',
+          editUrl: 'https://github.com/yokinanya/0StarFeathers_Docs/tree/master',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
